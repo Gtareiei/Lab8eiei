a33a63911a767bdd482cda260da86626134e00fb
Gtareiei <Natthapong0848@gmail.com>
2024-12-07 02:00:04 +0700

    lab8_3complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,48 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+using namespace std ; 
+int main() 
+{
+    int age , height , bou ; 
+cout<<"Enter your age: " ;
+cin>>age ;
+     if (age<=25)
+     {
+cout<<"Enter your height: " ;
+cin>>height;
+
+         if(height<100)
+         {
+         cout<<"Your character = Chopper" ;
+         }
+         else if (height<180)
+          {
+            cout<<"Your character = Usopp";
+          }        
+         else{
+cout << "Enter your bounty: ";
+cin >> bou;
+          if(bou>1100000000) 
+           {         
+               cout<<"Your character = Zoro" ;
+            }
+             else 
+               cout<<"Your character = Sanji";
+         }
+     }
+     else if (age<=60)
+     {
+cout<<"Enter your bounty: " ;
+cin>>bou ;
+        if(bou>500000000)
+        {
+            cout<<"Your character = Jinbe";
+        }
+        else 
+        {
+        cout<<"Your character = Franky" ;
+        }
+     }else cout<<"Your character = Brook";
+}
